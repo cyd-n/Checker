80df60c0a8104e79c3fbdf60c32638ac9d90f90a
cyd-n <darklord1234938@gmail.com>
2026-05-01 21:31:11 +0200

    US-8:You  can win and lose

--- FontEnd/JavaScript/index.js
+++ FontEnd/JavaScript/index.js
@@ -3,18 +3,27 @@ function Checker(){
         renderKey: 0,
         size: 10,
         // Looks awfull
-        playerWhite: [
+        playerBlack: [
             {x : 2, y : 1, king: false},{x : 4, y : 1, king: false},{x : 6, y : 1, king: false},{x : 8, y : 1, king: false},{x : 10, y : 1, king: false},
             {x : 1, y : 2, king: false},{x : 3, y : 2, king: false},{x : 5, y : 2, king: false},{x : 7, y : 2, king: false},{x : 9, y : 2, king: false},
             {x : 2, y : 3, king: false},{x : 4, y : 3, king: false},{x : 6, y : 3, king: false},{x : 8, y : 3, king: false},{x : 10, y : 3, king: false},
             {x : 1, y : 4, king: false},{x : 3, y : 4, king: false},{x : 5, y : 4, king: false},{x : 7, y : 4, king: false},{x : 9, y : 4, king: false}
         ],
 
-        playerBlack: [
+        playerWhite: [
+            {x : 1, y : 10, king: false}
+        ],
+
+        playerBackUp: [[
             {x : 1, y : 10, king: false},{x : 3, y : 10, king: false},{x : 5, y : 10, king: false},{x : 7, y : 10, king: false},{x : 9, y : 10, king: false},
             {x : 2, y : 9, king: false},{x : 4, y : 9, king: false},{x : 6, y : 9, king: false},{x : 8, y : 9, king: false},{x : 10, y : 9, king: false},
             {x : 1, y : 8, king: false},{x : 3, y : 8, king: false},{x : 5, y : 8, king: false},{x : 7, y : 8, king: false},{x : 9, y : 8, king: false},
             {x : 2, y : 7, king: false},{x : 4, y : 7, king: false},{x : 6, y : 7, king: false},{x : 8, y : 7, king: false},{x : 10, y : 7, king: false}
+        ], [
+            {x : 2, y : 1, king: false},{x : 4, y : 1, king: false},{x : 6, y : 1, king: false},{x : 8, y : 1, king: false},{x : 10, y : 1, king: false},
+            {x : 1, y : 2, king: false},{x : 3, y : 2, king: false},{x : 5, y : 2, king: false},{x : 7, y : 2, king: false},{x : 9, y : 2, king: false},
+            {x : 2, y : 3, king: false},{x : 4, y : 3, king: false},{x : 6, y : 3, king: false},{x : 8, y : 3, king: false},{x : 10, y : 3, king: false},
+            {x : 1, y : 4, king: false},{x : 3, y : 4, king: false},{x : 5, y : 4, king: false},{x : 7, y : 4, king: false},{x : 9, y : 4, king: false}]
         ],
 
         turn: {
@@ -29,7 +38,10 @@ function Checker(){
 
         seletedCount: 0,
 
-        winstate: {
+        showPopup: false,
+        popupTitle: "",
+
+        winState: {
             NONE  : 0,
             WHITE : 1,
             BLACK : 2
@@ -38,6 +50,8 @@ function Checker(){
         currWinState: 0,
 
         GetSeleted(_row, _col) {
+            this.WinStateFunc();
+
             switch(this.currTurn){ 
                 case 0: this.NextTurn(); break;
                 case 1: this.SeletedPiece(_row, _col, this.playerWhite); break;
@@ -75,7 +89,9 @@ function Checker(){
                 _pieces[this.seleted.i].x = _col;
                 _pieces[this.seleted.i].y = _row;
 
-                if(_pieces[this.seleted.i].y == 1){ _pieces[this.seleted.i].king = true; }
+                console.log(_pieces[this.seleted.i].y);
+
+                if(_pieces[this.seleted.i].y == 1 && this.currTurn == this.turn.WHITE || _pieces[this.seleted.i].y == 10 && this.currTurn == this.turn.BLACK){ _pieces[this.seleted.i].king = true; }
             }
 
             this.seletedCount = 0;
@@ -92,7 +108,7 @@ function Checker(){
                     let x = piece.x + dirs[d][0];
                     let y = piece.y + dirs[d][1];
 
-                    if(this.CanCapture(x, y, piece, _enemyPieces) >= 0){ return true; }
+                    if(this.CanCapture(x, y, piece, _enemyPieces) >= 0){ if(this.InField(x, y)) { console.log(x + "," + y); return true; } else {console.log("Outside:" + x + "," + y); } }
                 }
             }
 
@@ -104,7 +120,6 @@ function Checker(){
             this.seleted = {x:-1,y:-1,i:-1};
         },
 
-
         WhatIsSeleted(_row, _col, _pieces){ for(let i =0; i < _pieces.length; i++) { if(this.IsAt(_col, _row, _pieces[i])){ this.seleted.x = _col; this.seleted.y = _row; this.seleted.i = i; } } },
 
         GetColor(_row, _col) { return (_row == this.seleted.y && _col == this.seleted.x) ? "w-16 h-16 bg-blue border-4 border-blue" : ((_row + _col) % 2 === 0) ? "w-16 h-16 bg-white border-4 border-white" : "w-16 h-16 bg-black border-4 border-black"},
@@ -114,6 +129,23 @@ function Checker(){
             else { for (let i = 0; i < this.playerBlack.length; i++) { if(this.playerBlack[i].y == _row && this.playerBlack[i].x == _col){ this.seleted.x = 0; this.seleted.y = 0; if(this.playerBlack[i].king){return "Imgs/BlackKing.png";} else {return "Imgs/Black.png";} } } }
                 
             return "";
+        },
+
+        WinStateFunc(){
+            if(this.playerWhite.length < 1) { this.currWinState = this.winState.BLACK; this.popupTitle = "Player Black Won"; this.showPopup = true;}
+            else if(this.playerBlack.length < 1) { this.currWinState = this.winState.WHITE; this.popupTitle = "Player White Won"; this.showPopup = true;}
+            else if(!this.CanMove(this.playerWhite)) {this.currWinState = this.winState.BLACK; this.popupTitle = "Player Black Won"; this.showPopup = true;}
+            else if(!this.CanMove(this.playerBlack)) {this.currWinState = this.winState.WHITE; this.popupTitle = "Player White Won"; this.showPopup = true;}
+        },
+
+        ReStart(){
+            this.playerWhite = this.playerBackUp[0];
+            this.playerBlack = this.playerBackUp[1];
+
+            this.currTurn = this.turn.WHITE;
+
+            this.popupTitle = "";
+            this.showPopup = false;
         },
 
         GetTurn() { return (this.currTurn == this.turn.BLACK) ? "Black's Turn" : (this.currTurn == this.turn.WHITE) ? "White's Turn" : "None's Turn"; },
@@ -199,6 +231,32 @@ function Checker(){
 
         IsAt(_x, _y, _player) { return _player.x === _x && _player.y === _y; },
 
+        CanMove(_pieces){
+            let enemys = this.GetEnemy(_pieces);
+
+            for(let i =0; i < _pieces.length; i++){
+                if(_pieces == this.playerBlack){
+                    if(this.IsStanding(_pieces[i].x + 1, _pieces[i].y + 1) || this.IsStanding(_pieces[i].x - 1, _pieces[i].y + 1) || this.InField(_pieces[i].x + 1, _pieces[i].y + 1) || this.InField(_pieces[i].x - 1, _pieces[i].y + 1)){
+                        console.log("Black Can move");
+                        return true;
+                    }
+                }
+                else if(_pieces == this.playerWhite){
+                    if(this.IsStanding(_pieces[i].x + 1, _pieces[i].y - 1) || this.IsStanding(_pieces[i].x - 1, _pieces[i].y - 1) || this.InField(_pieces[i].x + 1, _pieces[i].y - 1) || this.InField(_pieces[i].x - 1, _pieces[i].y - 1)){
+                        console.log("White Can move");
+                        return true;
+                    }
+                }
+
+                if(!this.CanCapture(_pieces[i].x + 2, _pieces[i].y + 2, _pieces[i], enemys) || !this.CanCapture(_pieces[i].x - 2, _pieces[i].y + 2, _pieces[i], enemys)|| !this.CanCapture(_pieces[i].x - 2, _pieces[i].y - 2, _pieces[i], enemys)|| !this.CanCapture(_pieces[i].x + 2, _pieces[i].y - 2, _pieces[i], enemys)){
+                    console.log("Can Jump");
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
         IsStanding(_x, _y) { 
             for(let i = 0; i < this.playerBlack.length; i++) { if (this.playerBlack[i].y == _y && this.playerBlack[i].x == _x) { return false; } } 
             for(let i = 0; i < this.playerWhite.length; i++) { if (this.playerWhite[i].y == _y && this.playerWhite[i].x == _x) { return false; } } 
@@ -206,13 +264,13 @@ function Checker(){
             return true;
         },
 
-        InField(_x, _y) {  if (_y > this.size || _y < 1 || _x > this.size || _x < 0) { return false; } return true; },
+        InField(_x, _y) {  if (_y > this.size || _y < 1 || _x > this.size || _x < 1) { return false; } return true; },
 
         InBound(_x, _y, _player) { 
             console.log(this.currTurn);
 
-            if(this.currTurn == this.turn.BLACK) { console.log("b"); if (_player.y - 1 == _y) {  if(_player.x - 1 == _x || _player.x + 1 == _x) { return true; } } } 
-            else if(this.currTurn == this.turn.WHITE) { console.log("w"); if (_player.y + 1 == _y) {  if(_player.x - 1 == _x || _player.x + 1 == _x) { return true; } } }
+            if(this.currTurn == this.turn.WHITE) { console.log("b"); if (_player.y - 1 == _y) {  if(_player.x - 1 == _x || _player.x + 1 == _x) { return true; } } } 
+            else if(this.currTurn == this.turn.BLACK) { console.log("w"); if (_player.y + 1 == _y) {  if(_player.x - 1 == _x || _player.x + 1 == _x) { return true; } } }
 
             console.log("false");
 
@@ -236,14 +294,12 @@ function Checker(){
         TryCapture(_col, _row, _piece, _enemyPieces) {
             i = this.CanCapture(_col, _row, _piece, _enemyPieces);
 
-            if(i > 0) {
+            if(i >= 0) {
                 _enemyPieces.splice(i, 1);
                 _piece.x = _col;
                 _piece.y = _row;
                 return true;
             }
-
-            console.log("captured");
 
             return false;
         },
