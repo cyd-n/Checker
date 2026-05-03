7a395594b486f4a24498b0b9d17dab430b605c72
cyd-n <darklord1234938@gmail.com>
2026-05-03 03:24:13 +0200

    add a BackEnd

---
+++ Checkers/Checker/BackEnd/BackEnd/Migrations/20260424182548_InitCreationV2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreationV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Seed = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_matches", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "matches");
+        }
+    }
+}
---
+++ Checkers/Checker/BackEnd/BackEnd/Models/MatchContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models{
+    public class MatchContext : DbContext {
+        public DbSet<Matches> matches { get; set; }
+
+        public MatchContext(DbContextOptions<MatchContext> _options) : base(_options)
+        {
+
+        }
+    }
+}
